35f74bf1eaa5e2e9e363f5959aa90d9d7bd82c17
Eric Hong <eric.hong@intelicare.com.au>
2022-12-28 16:19:27 +0800

    push

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,29 @@
+import Link from "next/link"
+import { useRouter } from "next/router"
+
 function Home() {
-  return <h1>Home Page</h1>
+
+  const router = useRouter()
+
+  const handleClick = () => {
+    console.log('Placing your order')
+    router.push('/product')
+  }
+
+  return(
+    <div>
+      <h1>Home Page</h1>
+      <Link href='/blog'>
+        <p>Blog</p>
+      </Link>
+      <Link href='/product'>
+        <p>Products</p>
+      </Link>
+      <button onClick={handleClick}>
+        Place Order
+      </button>
+    </div>
+  ) 
 }
 
 export default Home
--- pages/product/index.tsx
+++ pages/product/index.tsx
@@ -1,9 +1,27 @@
-function ProductList() {
+import Link from "next/link"
+
+function ProductList( {productId = 100}) {
   return ( 
     <>
+    <Link href='/'>
+      <h1>Home</h1>
+    </Link>
+
+    <Link href='/product/1'>
       <h2>Product 1</h2>
+    </Link>
+
+    <Link href='/product/2'>
       <h2>Product 2</h2>
+    </Link>  
+    
+    <Link href='/product/3'>
       <h2>Product 3</h2>
+    </Link>
+
+    <Link href={`/product/${productId}`}>
+      <h2>Product {productId}</h2>
+    </Link>
     </>
   )
 }
