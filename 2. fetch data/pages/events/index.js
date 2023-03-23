09fa878cf18fe721289cf98607337583ebd3dde6
Eric Hong <eric.hong@intelicare.com.au>
2023-03-23 13:40:49 +0800

    1

--- 2. fetch data/pages/events/index.js
+++ 2. fetch data/pages/events/index.js
@@ -1,13 +1,13 @@
 import { Fragment } from 'react';
 import { useRouter } from 'next/router';
 
-import { getAllEvents } from '../../dummy-data';
+import { getAllEvents } from '../../helpers/api-utils';
 import EventList from '../../components/events/event-list';
 import EventsSearch from '../../components/events/events-search';
 
-function AllEventsPage() {
+function AllEventsPage(props) {
     const router = useRouter();
-    const events = getAllEvents();
+    const events = props.events // getAllEvents();
 
     function findEventsHandler(year, month) {
         const fullPath = `/events/${year}/${month}`;
@@ -22,5 +22,17 @@ function AllEventsPage() {
         </Fragment>
     );
 }
+
+export async function getStaticProps() {
+    const events = await getAllEvents();
+
+    return {
+        props: {
+            events: events
+        },
+        revalidate: 60
+    }
+}
+
 
 export default AllEventsPage;
