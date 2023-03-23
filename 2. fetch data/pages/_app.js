ef64e7ce5fcc223d530c04992c4db55f52efc35b
Eric Hong <eric.hong@intelicare.com.au>
2023-03-23 12:59:01 +0800

    2

--- 2. fetch data/pages/_app.js
+++ 2. fetch data/pages/_app.js
@@ -1,7 +1,12 @@
 import '@/styles/globals.css'
+import Layout from '../components/layout/layout';
 
 const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
 export default App;
