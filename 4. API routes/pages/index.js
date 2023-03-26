e7646f797edb588961dfd8357b08caf6544b5c74
eric-0815 <67636157+eric-0815@users.noreply.github.com>
2023-03-26 14:22:08 +0800

    4

--- 4. API routes/pages/index.js
+++ 4. API routes/pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 
-import { getFeaturedEvents } from '../helpers/api-utils';
+import { getFeaturedEvents } from '../helpers/api-util';
 import EventList from '../components/events/event-list';
 import NewsletterRegistration from '../components/input/newsletter-registration';
 
